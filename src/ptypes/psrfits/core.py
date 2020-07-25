6d3404835314f1a369ed70bdb0a386dc77def91b
Ujjwal Panda <ujjwalpanda97@gmail.com>
2020-07-25 19:45:48 +0530

    Added constants for PSRFITS.

--- src/ptypes/psrfits/core.py
+++ src/ptypes/psrfits/core.py
@@ -1,3 +1,34 @@
+import re
+import os
+
+import numpy as np
+import astropy.units as u
+import astropy.coordinates as coord
+
+from pathlib import Path
+from astropy.io import fits
+
 class PTypePSRFITS(PType):
 
-    def __init__(self): pass
+    def __init__(self,
+                 fname):
+
+        super().__init__(fname)
+
+        self.read()
+
+    def __readSubInt__(self,
+                       isub,
+                       scales=True,
+                       offsets=True,
+                       weights=True,
+                       zerooff=True,
+                       collapse=True):
+
+        pass
+
+    def read(self):
+
+        with fits.open(self.fname) as infile:
+
+            pass
