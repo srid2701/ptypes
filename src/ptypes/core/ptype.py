1b3c817b0c13df04d0a7faf05910143f876b5820
Ujjwal <ujjwalpanda97@gmail.com>
2020-09-28 21:25:22 +0530

    Starting type annotation for ptypes with the core module.

--- src/ptypes/core/ptype.py
+++ src/ptypes/core/ptype.py
@@ -2,12 +2,12 @@ import pprint
 
 
 class PType(object):
-    def __init__(self, fname):
+    def __init__(self, fname: str):
 
-        self.fname = fname
+        self.fname: str = fname
 
-    def __str__(self):
+    def __str__(self) -> str:
         return "{}\n{}".format(self.__class__, pprint.pformat(self.__dict__))
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return str(self)
